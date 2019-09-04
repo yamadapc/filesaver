a9a3d3b8d63853f4fd85f8e7f37545b667b82b64
yamadapc <tacla.yamada@gmail.com>
2019-09-04 21:37:33 +1000

    Add boilerplate to start visualizations work

---
+++ FileSaver/FileSaver/controllers/VisualizationsViewController.h
@@ -0,0 +1,14 @@
+//
+// Created by Pedro Tacla Yamada on 2019-09-04.
+// Copyright (c) 2019 Pedro Tacla Yamada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import <WebKit/WebKit.h>
+
+@interface VisualizationsViewController : NSViewController
+
+@property(weak) IBOutlet WKWebView *webview;
+
+@end
---
+++ FileSaver/FileSaver/controllers/VisualizationsViewController.mm
@@ -0,0 +1,12 @@
+//
+// Created by Pedro Tacla Yamada on 2019-09-04.
+// Copyright (c) 2019 Pedro Tacla Yamada. All rights reserved.
+//
+
+#import "VisualizationsViewController.h"
+
+
+@implementation VisualizationsViewController {
+
+}
+@end
