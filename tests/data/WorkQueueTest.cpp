5da376e30c4a891f1635170103486ab12330a6d1
yamadapc <tacla.yamada@gmail.com>
2020-02-27 07:32:25 +1100

    clang-format tests

--- tests/data/WorkQueueTest.cpp
+++ tests/data/WorkQueueTest.cpp
@@ -10,59 +10,62 @@
 
 #include "../../src/data/WorkQueue.h"
 
-template <typename T> bool isReady(std::future<T> &future) {
-  return future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+template <typename T> bool isReady (std::future<T>& future)
+{
+    return future.wait_for (std::chrono::seconds (0)) == std::future_status::ready;
 }
 
-TEST(WorkQueueTest, CanPushItemsThenPopThemOut) {
-  filesaver::WorkQueue<std::string> workQueue;
-  EXPECT_EQ(workQueue.size(), 0);
-  workQueue.push("Something interesting");
-  EXPECT_EQ(workQueue.size(), 1);
-  EXPECT_EQ(workQueue.front(), "Something interesting");
-  EXPECT_EQ(workQueue.size(), 0);
+TEST (WorkQueueTest, CanPushItemsThenPopThemOut)
+{
+    filesaver::WorkQueue<std::string> workQueue;
+    EXPECT_EQ (workQueue.size (), 0);
+    workQueue.push ("Something interesting");
+    EXPECT_EQ (workQueue.size (), 1);
+    EXPECT_EQ (workQueue.front (), "Something interesting");
+    EXPECT_EQ (workQueue.size (), 0);
 }
 
-TEST(WorkQueueTest, CanWaitUntilItemsAreAdded) {
-  filesaver::WorkQueue<std::string> workQueue;
-  workQueue.push("Amazing 1");
-  workQueue.push("Amazing 2");
+TEST (WorkQueueTest, CanWaitUntilItemsAreAdded)
+{
+    filesaver::WorkQueue<std::string> workQueue;
+    workQueue.push ("Amazing 1");
+    workQueue.push ("Amazing 2");
 
-  {
-    auto future1 = std::async([&] {
-      usleep(100000);
-      auto f = workQueue.front();
-      return f;
-    });
+    {
+        auto future1 = std::async ([&] {
+            usleep (100000);
+            auto f = workQueue.front ();
+            return f;
+        });
 
-    auto future2 = std::async([&] {
-      usleep(200000);
-      auto f = workQueue.front();
-      return f;
-    });
+        auto future2 = std::async ([&] {
+            usleep (200000);
+            auto f = workQueue.front ();
+            return f;
+        });
 
-    auto future3 = std::async([&] {
-      usleep(300000);
-      auto f = workQueue.front();
-      return f;
-    });
+        auto future3 = std::async ([&] {
+            usleep (300000);
+            auto f = workQueue.front ();
+            return f;
+        });
 
-    usleep(210000);
+        usleep (210000);
 
-    EXPECT_TRUE(isReady(future1));
-    EXPECT_TRUE(isReady(future2));
-    EXPECT_FALSE(isReady(future3));
-    EXPECT_EQ(future1.get(), "Amazing 1");
-    EXPECT_EQ(future2.get(), "Amazing 2");
-    EXPECT_EQ(workQueue.size(), 0);
+        EXPECT_TRUE (isReady (future1));
+        EXPECT_TRUE (isReady (future2));
+        EXPECT_FALSE (isReady (future3));
+        EXPECT_EQ (future1.get (), "Amazing 1");
+        EXPECT_EQ (future2.get (), "Amazing 2");
+        EXPECT_EQ (workQueue.size (), 0);
 
-    usleep(300000);
-    EXPECT_FALSE(isReady(future3));
+        usleep (300000);
+        EXPECT_FALSE (isReady (future3));
 
-    workQueue.push("Amazing 3");
-    usleep(100000);
-    EXPECT_EQ(workQueue.size(), 0);
-    EXPECT_TRUE(isReady(future3));
-    EXPECT_EQ(future3.get(), "Amazing 3");
-  }
+        workQueue.push ("Amazing 3");
+        usleep (100000);
+        EXPECT_EQ (workQueue.size (), 0);
+        EXPECT_TRUE (isReady (future3));
+        EXPECT_EQ (future3.get (), "Amazing 3");
+    }
 }
--- tests/models/FileEntryTest.cpp
+++ tests/models/FileEntryTest.cpp
@@ -9,69 +9,72 @@
 
 #include "../../src/models/FileEntry.h"
 
-TEST(FileEntryTest, CanTellTheSizeOfFiles) {
-  auto tmp = boost::filesystem::temp_directory_path();
-  tmp.append("test-file.txt");
-  {
-    boost::filesystem::ofstream outputStream(tmp);
-    outputStream << "some stuff here";
-  }
+TEST (FileEntryTest, CanTellTheSizeOfFiles)
+{
+    auto tmp = boost::filesystem::temp_directory_path ();
+    tmp.append ("test-file.txt");
+    {
+        boost::filesystem::ofstream outputStream (tmp);
+        outputStream << "some stuff here";
+    }
 
-  EXPECT_TRUE(boost::filesystem::exists(tmp));
-  auto entry = filesaver::FileEntry::fromPath(tmp.string());
-  auto size = entry->size;
+    EXPECT_TRUE (boost::filesystem::exists (tmp));
+    auto entry = filesaver::FileEntry::fromPath (tmp.string ());
+    auto size = entry->size;
 
-  EXPECT_GT(size, 0);
-  EXPECT_EQ(size, boost::filesystem::file_size(tmp));
+    EXPECT_GT (size, 0);
+    EXPECT_EQ (size, boost::filesystem::file_size (tmp));
 }
 
-TEST(FileEntryTest, CanTellTheSizeOfDirectories) {
-  auto tmp = boost::filesystem::temp_directory_path();
-  tmp.append("test-directory");
-  boost::filesystem::create_directory(tmp);
-  EXPECT_TRUE(boost::filesystem::exists(tmp));
+TEST (FileEntryTest, CanTellTheSizeOfDirectories)
+{
+    auto tmp = boost::filesystem::temp_directory_path ();
+    tmp.append ("test-directory");
+    boost::filesystem::create_directory (tmp);
+    EXPECT_TRUE (boost::filesystem::exists (tmp));
 
-  auto entry = filesaver::FileEntry::fromPath(tmp.string());
-  auto size = entry->size;
+    auto entry = filesaver::FileEntry::fromPath (tmp.string ());
+    auto size = entry->size;
 
-  EXPECT_GT(size, 0);
-  EXPECT_EQ(entry->isDirectory(), true);
+    EXPECT_GT (size, 0);
+    EXPECT_EQ (entry->isDirectory (), true);
 }
 
-TEST(FileEntryTest, CanListDirectoryChildren) {
-  auto tmp = boost::filesystem::temp_directory_path();
-  tmp.append("test-directory");
-  boost::filesystem::create_directory(tmp);
-  EXPECT_TRUE(boost::filesystem::exists(tmp));
+TEST (FileEntryTest, CanListDirectoryChildren)
+{
+    auto tmp = boost::filesystem::temp_directory_path ();
+    tmp.append ("test-directory");
+    boost::filesystem::create_directory (tmp);
+    EXPECT_TRUE (boost::filesystem::exists (tmp));
 
-  auto tmpFile1 = tmp;
-  tmpFile1.append("file1.txt");
-  auto tmpFile2 = tmp;
-  tmpFile2.append("file2.txt");
+    auto tmpFile1 = tmp;
+    tmpFile1.append ("file1.txt");
+    auto tmpFile2 = tmp;
+    tmpFile2.append ("file2.txt");
 
-  {
-    boost::filesystem::ofstream outputStream(tmpFile1);
-    outputStream << "some stuff here 123";
-  }
-  {
-    boost::filesystem::ofstream outputStream(tmpFile2);
-    outputStream << "some stuff here 123";
-  }
+    {
+        boost::filesystem::ofstream outputStream (tmpFile1);
+        outputStream << "some stuff here 123";
+    }
+    {
+        boost::filesystem::ofstream outputStream (tmpFile2);
+        outputStream << "some stuff here 123";
+    }
 
-  auto entry = filesaver::FileEntry::fromPath(tmp.string());
-  auto children = entry->children();
+    auto entry = filesaver::FileEntry::fromPath (tmp.string ());
+    auto children = entry->children ();
 
-  EXPECT_EQ(children.size(), 2);
+    EXPECT_EQ (children.size (), 2);
 
-  {
-    auto it = std::find(children.begin(), children.end(), tmpFile1.string());
-    EXPECT_NE(it, children.end());
-    EXPECT_EQ(it->string(), tmpFile1.string());
-  }
+    {
+        auto it = std::find (children.begin (), children.end (), tmpFile1.string ());
+        EXPECT_NE (it, children.end ());
+        EXPECT_EQ (it->string (), tmpFile1.string ());
+    }
 
-  {
-    auto it = std::find(children.begin(), children.end(), tmpFile2.string());
-    EXPECT_NE(it, children.end());
-    EXPECT_EQ(it->string(), tmpFile2.string());
-  }
+    {
+        auto it = std::find (children.begin (), children.end (), tmpFile2.string ());
+        EXPECT_NE (it, children.end ());
+        EXPECT_EQ (it->string (), tmpFile2.string ());
+    }
 }
