434055e4963886183a7ec9f4b2ffffd332cdd3e8
yamadapc <tacla.yamada@gmail.com>
2020-06-30 21:49:14 +1000

    Break-up FileSizeService.cpp

--- src/lfilesaver/services/filesize/FileSizeService.h
+++ src/lfilesaver/services/filesize/FileSizeService.h
@@ -8,67 +8,16 @@
 #include <memory>
 #include <optional>
 #include <unordered_map>
-#include <unordered_set>
 
 #include "../../data/WorkQueue.h"
 #include "../../models/FileEntry.h"
 #include "../storage/LevelDbStorageService.h"
 #include "../storage/StorageService.h"
 
+#include "InMemoryFileEntryStore.h"
+
 namespace filesaver::services
 {
-
-class InMemoryFileEntryStore
-{
-public:
-    class Delegate
-    {
-    public:
-        virtual ~Delegate ()
-        {
-        }
-
-        virtual void onPathFinished (std::shared_ptr<FileEntry> fileEntry) = 0;
-    };
-
-    struct Record
-    {
-        std::shared_ptr<FileEntry> fileEntry;
-        off_t totalSize;
-        unsigned long pendingChildren;
-    };
-
-    explicit InMemoryFileEntryStore (Delegate& storeDelegate);
-
-    /// Push an entry onto the in-memory store
-    void addEntry (std::shared_ptr<FileEntry> entry);
-
-    /// Clean entry from memory
-    void cleanEntry (const std::string& filepath);
-
-    /// Get the current in-memory size for a path
-    std::optional<off_t> getCurrentSizeAt (const std::string& filepath);
-
-    /// Return true if this path has been finished
-    bool isPathFinished (const std::string& filepath);
-
-private:
-    /// When an entry is pushed, update its finished state and all of its parents
-    void updatePendingAndFinishedState (const boost::filesystem::path& filepath);
-
-    /// When an entry is pushed, update its size and all of its parents
-    void updateSizes (const std::shared_ptr<filesaver::FileEntry>& entry);
-
-    /// Adds a size delta recursively to this path and its parents
-    void addSizeRecursive (const boost::filesystem::path& filepath, off_t sizeDiff);
-
-    /// Adds a size delta to this path and its parents
-    void addSize (const boost::filesystem::path& path, off_t sizeDiff);
-
-    std::unordered_map<std::string, Record> m_records;
-
-    Delegate& m_delegate;
-};
 
 class FileSizeService : public virtual InMemoryFileEntryStore::Delegate
 {
---
+++ src/lfilesaver/services/filesize/InMemoryFileEntryStore.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Pedro Tacla Yamada on 30/6/20.
+//
+
+
+#include "InMemoryFileEntryStore.h"
+
+namespace filesaver::services
+{
+
+InMemoryFileEntryStore::InMemoryFileEntryStore (Delegate& storeDelegate) : m_delegate (storeDelegate)
+{
+}
+
+void InMemoryFileEntryStore::addEntry (std::shared_ptr<FileEntry> entry)
+{
+    auto filepath = entry->filepath;
+    auto filepathStr = entry->filepath.string ();
+
+    m_records[filepathStr] = {entry, 0, 0};
+    auto& record = m_records[filepathStr];
+
+    updateSizes (entry);
+
+    if (!entry->isDirectory ())
+    {
+        record.pendingChildren = 0;
+        updatePendingAndFinishedState (entry->filepath);
+    }
+    else
+    {
+        record.pendingChildren = entry->children ().size ();
+        if (record.pendingChildren == 0)
+        {
+            updatePendingAndFinishedState (entry->filepath);
+        }
+    }
+}
+
+void InMemoryFileEntryStore::cleanEntry (const std::string& filepath)
+{
+    m_records.erase (filepath);
+}
+
+void InMemoryFileEntryStore::updateSizes (const std::shared_ptr<filesaver::FileEntry>& entry)
+{
+    auto previousSize = getCurrentSizeAt (entry->filepath.string ()).value_or (0L);
+    auto sizeDiff = entry->size - previousSize;
+    addSizeRecursive (entry->filepath, sizeDiff);
+}
+
+std::optional<off_t> InMemoryFileEntryStore::getCurrentSizeAt (const std::string& filepath)
+{
+    auto totalSizeIt = m_records.find (filepath);
+    if (totalSizeIt == m_records.end ())
+    {
+        return std::optional<off_t>{};
+    }
+    return totalSizeIt->second.totalSize;
+}
+
+bool InMemoryFileEntryStore::isPathFinished (const std::string& filepath)
+{
+    auto pendingIt = m_records.find (filepath);
+    if (pendingIt == m_records.end ())
+    {
+        return false;
+    }
+    return pendingIt->second.pendingChildren <= 0;
+}
+
+void InMemoryFileEntryStore::addSizeRecursive (const boost::filesystem::path& filepath, off_t sizeDiff)
+{
+    addSize (filepath, sizeDiff);
+
+    // This is faster than using .has_parent_path or .parent_path because it'll
+    // do less copying/alloc.
+    boost::filesystem::path path = {filepath};
+    do
+    {
+        path = path.remove_filename ();
+        addSize (path, sizeDiff);
+    } while (!path.empty ());
+}
+
+void InMemoryFileEntryStore::updatePendingAndFinishedState (const boost::filesystem::path& filepath)
+{
+    const auto& filepathStr = filepath.string ();
+    auto entry = m_records[filepathStr].fileEntry;
+    entry->isFinished = true;
+    m_delegate.onPathFinished (entry);
+
+    if (filepath.has_parent_path ())
+    {
+        auto parentPath = filepath.parent_path ();
+        const auto& parentPathStr = parentPath.string ();
+        auto& record = m_records[parentPathStr];
+
+        record.pendingChildren -= 1;
+        if (record.pendingChildren <= 0)
+        {
+            updatePendingAndFinishedState (parentPath);
+        }
+    }
+}
+
+void InMemoryFileEntryStore::addSize (const boost::filesystem::path& path, off_t sizeDiff)
+{
+    auto parentIt = m_records.find (path.string ());
+    if (parentIt != m_records.end ())
+    {
+        parentIt->second.totalSize += sizeDiff;
+    }
+}
+
+}
