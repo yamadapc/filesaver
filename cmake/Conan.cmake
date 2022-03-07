c9b3723c576ad3b18387b4efdd3b140081b10613
yamadapc <tacla.yamada@gmail.com>
2022-03-07 21:48:31 +1100

    Fix compilation on M1 macbook

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -10,8 +10,8 @@ endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-conan_add_remote(NAME bincrafters URL
-                 https://api.bintray.com/conan/bincrafters/public-conan)
+conan_add_remote(NAME conancenter URL
+                 https://center.conan.io)
 
 conan_cmake_run(
   REQUIRES
